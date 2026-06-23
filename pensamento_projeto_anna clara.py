805cc57bc8561dc31bb50834406e4e788bb63bac
anna clara <ac6127432@gmail.com>
2026-06-23 16:34:16 -0300

    upgrade - repositorio com jovens

---
+++ pensamento_projeto_anna clara.py
@@ -0,0 +1,63 @@
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+
+#ufa, quebrei a maldição 
+# print('olá, mundo!')
+print('-'*48 + '\n')
+print('bem-vindo ao sistema de vendas-açaiteria!\n')
+print('1 - cadastrar produto')
+print('2 -  listar produtos')
+print('3 - realizar venda')
+print('4 - combo de açai')
+print('5 - bebidas')
+print('6 - sobre nós')
+print('7 - fale conosco')
+print('8 - envie seu feedback')
+print('9 - recomendações')
+print('0 - sair do sistema ')
+print('\n--------------------------------------------\n')
+
+opção__definida = int(input('Digite a opção desejada'))
+
+if opcao__definida == 1:
+    print('cadastrando produto...')
+    
+elif opcao__definida == 2:
+    print('Listando produtos...')
+
+
+elif opcao__definida == 3:
+     print('realizando venda...')
+
+elif opcao__definida == 4:
+     print('Combos de açai')
+
+elif opcao__definida == 5:
+    print('Bebidas...')
+
+elif opcao__definida == 6:
+    print('Sobre nós...')
+
+elif opcao__definida == 7:
+    print('Fale conosco...')
+
+elif opcao__definida == 8:
+    print('Envie seu feedback...')
+
+elif opcao__definida == 9:
+    print('Recomendações...')
+    
+else: print('A intereção falhou, tente novamente')
